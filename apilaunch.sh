e1e5e0b91a5dd220495a65fee9c81ec0a44f936a
Negative <negative_amir@yahoo.com>
2016-05-29 16:53:59 +0430

    Update apilaunch.sh

--- apilaunch.sh
+++ apilaunch.sh
@@ -112,7 +112,7 @@ else
   fi
   while true; do
    rm -r ../.telegram-cli/state
-   ./tg/bin/telegram-cli -b ./tg/tg-server.pub -s ./bot/seedbot.lua -l 1 -E $@
+   ./tg/bin/telegram-cli -b ./tg/tg-server.pub -s ./bot/taylor-bot.lua -l 1 -E $@
    sleep 3
   done
 fi
