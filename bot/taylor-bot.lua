2ff0ef9aeee56fe48642ebed15321e3b00b230c4
GSRMOHAMMAD <mamadbt19@gmail.com>
2016-07-01 11:48:16 -0800

    Update taylor-bot.lua

--- bot/taylor-bot.lua
+++ bot/taylor-bot.lua
@@ -233,7 +233,7 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {68747297},--Sudo users
+    sudo_users = {90516804,172561830},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Taylor Team v4
     Taylor Team and Taylor Bot Anti spam / anti link
