2b0b9093e1f0ccbb27d79d65fbc8e786d3d2ee60
GSRMOHAMMAD <mamadbt19@gmail.com>
2016-07-02 13:54:15 -0800

    Update lockfwd.lua

--- plugins/lockfwd.lua
+++ plugins/lockfwd.lua
@@ -23,11 +23,11 @@ local function run(msg, matches)
             
                     local hash = 'mate:'..msg.to.id
                     redis:set(hash, true)
-                    return "قفل فروارد فعال شد"
+                    return "😊قفل فروارد فعال شد"
   elseif is_momod(msg) and matches[1] == 'unlock' then
                     local hash = 'mate:'..msg.to.id
                     redis:del(hash)
-                    return "قفل فروارد غیر فعال شد"
+                    return "😐قفل فروارد غیر فعال شد"
 end
 
 end
